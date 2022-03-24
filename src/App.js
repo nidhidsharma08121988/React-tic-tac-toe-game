ad9e5a6aaaa68a6526cf91d8ccaa3abcdbe5e33a
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-24 11:38:27 +0100

    Fix: header in App.js and css in App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Board from './components/Board'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div className='title'>Tic Tac Toe Game</div>
       </header>
+      <Board rows='3' cols='3' />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Board.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Board = ({ rows, cols }) => {
+  return <div>Board</div>
+}
+
+export default Board
