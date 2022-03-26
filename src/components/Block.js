2a5b0ad4665bf4bf5d9d3781a6e60b700f2100d6
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-26 12:18:18 +0100

    test: pass test, No duplicate selection of block

--- src/components/Block.js
+++ src/components/Block.js
@@ -2,11 +2,12 @@ import React, { useState } from 'react'
 
 const Block = ({ colId, rowId, turns, setTurns }) => {
   const [value, setValue] = useState('')
-
+  const [disable, setDisable] = useState(false)
   const handleBlockClick = e => {
     if (turns % 2 === 0) setValue('X')
     else setValue('O')
     setTurns(turns + 1)
+    setDisable(true)
   }
   return (
     <button
@@ -14,6 +15,7 @@ const Block = ({ colId, rowId, turns, setTurns }) => {
       id={`${rowId}${colId}`}
       data-testid={`${rowId}${colId}`}
       onClick={handleBlockClick}
+      //   disabled={disable}
     >
       {value}
     </button>
