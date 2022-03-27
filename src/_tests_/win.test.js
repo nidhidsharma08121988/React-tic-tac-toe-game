295bf536fc0cb78c40f67eb7615be34c81ef78d6
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-27 14:54:43 +0200

    test(fix): fix test to make boardState an object

--- src/_tests_/win.test.js
+++ src/_tests_/win.test.js
@@ -4,17 +4,17 @@ import Board from '../components/Board'
 describe('Game won', () => {
   describe('Player X wins', () => {
     test('Consecutive X in a row', () => {
-      const boardState = [
-        { R1C1: 'X' },
-        { R1C2: 'X' },
-        { R1C3: 'X' },
-        { R2C1: 'O' },
-        { R2C2: '' },
-        { R2C3: '' },
-        { R3C1: '' },
-        { R3C1: '' },
-        { R3C1: '' },
-      ]
+      const boardState = {
+        R1C1: 'X',
+        R1C2: 'X',
+        R1C3: 'X',
+        R2C1: 'O',
+        R2C2: '',
+        R2C3: '',
+        R3C1: '',
+        R3C2: '',
+        R3C3: '',
+      }
       render(<Board boardState={boardState} rows='3' cols='3' />)
 
       expect(screen.getByText('Winner is X')).toBeTruthy()
