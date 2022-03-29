2da89522f7a5e6420b07ff83e53bb3cd70f8a5f5
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-29 09:52:43 +0200

    refactor(Board.js): To allow outcome to be displayed

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import RowsInBoard from './RowsInBoard'
+import { GameContext } from './../store/GameState'
 
 const Board = ({ rows, cols }) => {
+  const { outcome } = useContext(GameContext)
   const rowArr = [...Array(Number.parseInt(rows))]
-  return <RowsInBoard rowArr={rowArr} cols={cols} />
+  return (
+    <div className='game-board'>
+      <div className='board'>
+        <RowsInBoard rowArr={rowArr} cols={cols} />
+      </div>
+      <div className='outcome'>{outcome}</div>
+    </div>
+  )
 }
 
 export default Board
