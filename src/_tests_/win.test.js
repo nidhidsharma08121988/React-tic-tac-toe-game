0576f27712c3a2d205675fd60fd0fb9b828ad4ad
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-29 19:09:41 +0200

    test(failed): winner is X when consecutive X are in row 2

--- src/_tests_/win.test.js
+++ src/_tests_/win.test.js
@@ -20,6 +20,23 @@ describe('Game won', () => {
 
       expect(screen.getByText('Winner is X')).toBeTruthy()
     })
+    test('Consecutive X in row 2', () => {
+      render(
+        <GameState>
+          <Board rows='3' cols='3' />
+        </GameState>
+      )
+
+      for (let col = 1; col <= 3; col++) {
+        const xBtn = screen.getByTestId(`R2C${col}`)
+        xBtn.click()
+        const oBtn = screen.getByTestId(`R3C${col}`)
+        oBtn.click()
+      }
+
+      expect(screen.getByText('Winner is X')).toBeTruthy()
+    })
+
     test('Consecutive X in a column', () => {})
     test('Consecutive X diagnally', () => {})
   })
