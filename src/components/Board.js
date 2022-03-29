bbc8706139c01c37f8b8cc0bdb6645f46b9ba366
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-29 19:19:54 +0200

    test(pass): winner is x when consecutive x in row 3

--- src/components/Board.js
+++ src/components/Board.js
@@ -43,13 +43,13 @@ function checkOIsWinner(boardState) {
 function checkXIsWinning(boardState) {
   return (
     hasWinningCombination(boardState, XInSameRow1) ||
-    hasWinningCombination(boardState, XInSameRow2)
+    hasWinningCombination(boardState, XInSameRow2) ||
+    hasWinningCombination(boardState, XInSameRow3)
   )
   // hasWinningCombination(boardState, XInSameCol1) ||
   // hasWinningCombination(boardState, XInSameCol2) ||
   // hasWinningCombination(boardState, XInSameCol3) ||
-  //
-  // hasWinningCombination(boardState, XInSameRow3) ||
+
   // hasWinningCombination(boardState, XDiagonalL) ||
   // hasWinningCombination(boardState, XDiagonalR)
 }
