8bb5ef5cb04b1441050858152b4fa127649a196b
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-29 10:40:43 +0200

    fix and refactor: useEffect logic must be in the Board component. * destructure and extract the different possible  winning combinations from the winningCombinationX and winningCombinationY * Update the boardContainsObject file to contain hasWinningCombination method such that it takes the objects destructured as second parameter

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,10 +1,57 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import RowsInBoard from './RowsInBoard'
 import { GameContext } from './../store/GameState'
+import winningCombinationO from '../store/winningCombinationO'
+import winningCombinationX from '../store/winningCombinationX'
+import hasWinningCombination from '../store/boardContainsObject'
 
 const Board = ({ rows, cols }) => {
-  const { outcome } = useContext(GameContext)
+  const { outcome, boardState, setOutcome } = useContext(GameContext)
   const rowArr = [...Array(Number.parseInt(rows))]
+  const {
+    XInSameCol1,
+    XInSameCol2,
+    XInSameCol3,
+    XInSameRow1,
+    XInSameRow2,
+    XInSameRow3,
+    XDiagonal,
+  } = winningCombinationX
+
+  const {
+    OInSameCol1,
+    OInSameCol2,
+    OInSameCol3,
+    OInSameRow1,
+    OInSameRow2,
+    OInSameRow3,
+    ODiagonal,
+  } = winningCombinationO
+
+  useEffect(() => {
+    const XIsWinning =
+      hasWinningCombination(boardState, XInSameCol1) ||
+      hasWinningCombination(boardState, XInSameCol2) ||
+      hasWinningCombination(boardState, XInSameCol3) ||
+      hasWinningCombination(boardState, XInSameRow1) ||
+      hasWinningCombination(boardState, XInSameRow2) ||
+      hasWinningCombination(boardState, XInSameRow3) ||
+      hasWinningCombination(boardState, XDiagonal)
+    const OIsWinning =
+      hasWinningCombination(boardState, OInSameCol1) ||
+      hasWinningCombination(boardState, OInSameCol2) ||
+      hasWinningCombination(boardState, OInSameCol3) ||
+      hasWinningCombination(boardState, OInSameRow1) ||
+      hasWinningCombination(boardState, OInSameRow2) ||
+      hasWinningCombination(boardState, OInSameRow3) ||
+      hasWinningCombination(boardState, ODiagonal)
+
+    if (XIsWinning) setOutcome('Winner is X')
+    else if (OIsWinning) setOutcome('Winner is O')
+    else setOutcome('')
+    //eslint-disable-next-line
+  }, [boardState])
+
   return (
     <div className='game-board'>
       <div className='board'>
--- src/store/boardContainsObject.js
+++ src/store/boardContainsObject.js
@@ -1,24 +1,11 @@
-const boardContainsObject = (boardState, winningCombination) => {
-  // console.log('winningCombination', winningCombination)
-  // console.log('key', Object.keys(winningCombination))
-  Object.keys(winningCombination).map(key => {
-    // console.log('object', winningCombination[key])
-    // console.log('is object', typeof winningCombination[key] === 'object')
-    if (typeof winningCombination[key] === 'object') {
-      const arrayofKeyValue = Object.entries(winningCombination[key])
-      return arrayofKeyValue.every(item => {
-        const key = item[0]
-        const value = item[1].value
-        console.log('item', item)
-        console.log('key', key)
-        console.log('value', value)
-        console.log('boardState', boardState[key])
-        console.log('boardState value', boardState[key].value)
-        return value === boardState[key].value
-      })
-    }
-    return false
-  })
+const hasWinningCombination = (boardState, winningCombination) => {
+  if (typeof winningCombination === 'object') {
+    const arrayofKeyValue = Object.entries(winningCombination)
+    return arrayofKeyValue.every(item => {
+      const key = item[0]
+      return item[1].value === boardState[key].value
+    })
+  }
 }
 
-export default boardContainsObject
+export default hasWinningCombination
