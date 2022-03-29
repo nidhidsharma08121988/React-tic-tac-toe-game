ca8019a64a29aef5ff9395b62778d5d02e36945f
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-29 09:57:01 +0200

    Update: add winning combination file to find outcome

---
+++ src/store/winningCombinationO.js
@@ -0,0 +1,80 @@
+const winningCombinationO = {
+  OInSameRow1: {
+    R1C1: {
+      value: 'O',
+    },
+    R1C2: {
+      value: 'O',
+    },
+    R1C3: {
+      value: 'O',
+    },
+  },
+  OInSameRow2: {
+    R2C1: {
+      value: 'O',
+    },
+    R2C2: {
+      value: 'O',
+    },
+    R2C3: {
+      value: 'O',
+    },
+  },
+  OInSameRow3: {
+    R3C1: {
+      value: 'O',
+    },
+    R3C2: {
+      value: 'O',
+    },
+    R3C3: {
+      value: 'O',
+    },
+  },
+  OInSameCol1: {
+    R1C1: {
+      value: 'O',
+    },
+    R2C1: {
+      value: 'O',
+    },
+    R3C1: {
+      value: 'O',
+    },
+  },
+  OInSameCol2: {
+    R1C2: {
+      value: 'O',
+    },
+    R2C2: {
+      value: 'O',
+    },
+    R3C2: {
+      value: 'O',
+    },
+  },
+  OInSameCol3: {
+    R1C3: {
+      value: 'O',
+    },
+    R2C3: {
+      value: 'O',
+    },
+    R3C3: {
+      value: 'O',
+    },
+  },
+  ODiagonal: {
+    R1C1: {
+      value: 'O',
+    },
+    R2C2: {
+      value: 'O',
+    },
+    R3C3: {
+      value: 'O',
+    },
+  },
+}
+export default winningCombinationO
---
+++ src/store/winningCombinationX.js
@@ -0,0 +1,80 @@
+const winningCombinationX = {
+  XInSameRow1: {
+    R1C1: {
+      value: 'X',
+    },
+    R1C2: {
+      value: 'X',
+    },
+    R1C3: {
+      value: 'X',
+    },
+  },
+  XInSameRow2: {
+    R2C1: {
+      value: 'X',
+    },
+    R2C2: {
+      value: 'X',
+    },
+    R2C3: {
+      value: 'X',
+    },
+  },
+  XInSameRow3: {
+    R3C1: {
+      value: 'X',
+    },
+    R3C2: {
+      value: 'X',
+    },
+    R3C3: {
+      value: 'X',
+    },
+  },
+  XInSameCol1: {
+    R1C1: {
+      value: 'X',
+    },
+    R2C1: {
+      value: 'X',
+    },
+    R3C1: {
+      value: 'X',
+    },
+  },
+  XInSameCol2: {
+    R1C2: {
+      value: 'X',
+    },
+    R2C2: {
+      value: 'X',
+    },
+    R3C2: {
+      value: 'X',
+    },
+  },
+  XInSameCol3: {
+    R1C3: {
+      value: 'X',
+    },
+    R2C3: {
+      value: 'X',
+    },
+    R3C3: {
+      value: 'X',
+    },
+  },
+  XDiagonal: {
+    R1C1: {
+      value: 'X',
+    },
+    R2C2: {
+      value: 'X',
+    },
+    R3C3: {
+      value: 'X',
+    },
+  },
+}
+export default winningCombinationX
