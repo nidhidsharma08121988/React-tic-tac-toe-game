a601afd65c4f88444a4dd34eaffe26fc2143072d
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-30 09:22:01 +0200

    test(failed): add test for winner is x when consecutive X are in col 1

--- src/_tests_/win.test.js
+++ src/_tests_/win.test.js
@@ -4,7 +4,7 @@ import GameState from '../store/GameState'
 
 describe('Game won', () => {
   describe('Player X wins', () => {
-    function consecutiveRowClick(xRowId, oRowId) {
+    const consecutiveRowClick = (xRowId, oRowId) => {
       for (let col = 1; col <= 3; col++) {
         const xBtn = screen.getByTestId(`${xRowId}C${col}`)
         xBtn.click()
@@ -12,6 +12,7 @@ describe('Game won', () => {
         oBtn.click()
       }
     }
+
     test('Consecutive X in row 1', () => {
       render(
         <GameState>
@@ -46,7 +47,21 @@ describe('Game won', () => {
       expect(screen.getByText('Winner is X')).toBeTruthy()
     })
 
-    test('Consecutive X in a column', () => {})
+    test('Consecutive X in a column 1', () => {
+      render(
+        <GameState>
+          <Board rows='3' cols='3' />
+        </GameState>
+      )
+
+      for (let row = 1; row <= 3; row++) {
+        const xBtn = screen.getByTestId(`R${row}C1`)
+        xBtn.click()
+        const oBtn = screen.getByTestId(`R${row}C3`)
+        oBtn.click()
+      }
+      expect(screen.getByText('Winner is X')).toBeTruthy()
+    })
     test('Consecutive X diagnally', () => {})
   })
 })
