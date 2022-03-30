9751fa672801f6889949dc2f7c553e4c2bf3e29f
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-03-30 09:23:20 +0200

    test(pass): Fix Board.js to allow winner is x when consecutive x are in col 1

--- src/components/Board.js
+++ src/components/Board.js
@@ -44,9 +44,10 @@ function checkXIsWinning(boardState) {
   return (
     hasWinningCombination(boardState, XInSameRow1) ||
     hasWinningCombination(boardState, XInSameRow2) ||
-    hasWinningCombination(boardState, XInSameRow3)
+    hasWinningCombination(boardState, XInSameRow3) ||
+    hasWinningCombination(boardState, XInSameCol1)
   )
-  // hasWinningCombination(boardState, XInSameCol1) ||
+  //  ||
   // hasWinningCombination(boardState, XInSameCol2) ||
   // hasWinningCombination(boardState, XInSameCol3) ||
 
