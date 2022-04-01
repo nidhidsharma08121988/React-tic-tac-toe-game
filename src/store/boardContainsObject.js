738b85d0a984e99dce6e75db9cd61af8b845d76d
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-01 15:19:35 +0200

    Refactor: refactor hasWinningCombination to remove checking typeof winningCombination as it is always object

--- src/store/boardContainsObject.js
+++ src/store/boardContainsObject.js
@@ -1,11 +1,9 @@
 const hasWinningCombination = (boardState, winningCombination) => {
-  if (typeof winningCombination === 'object') {
-    const arrayofKeyValue = Object.entries(winningCombination)
-    return arrayofKeyValue.every(item => {
-      const key = item[0]
-      return item[1].value === boardState[key].value
-    })
-  }
+  const arrayofKeyValue = Object.entries(winningCombination)
+  return arrayofKeyValue.every(item => {
+    const key = item[0]
+    return item[1].value === boardState[key].value
+  })
 }
 
 export default hasWinningCombination
