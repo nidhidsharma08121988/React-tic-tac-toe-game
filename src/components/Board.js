ab33e46e85d1da809d77eb24c4420e0d056ad6e1
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-02 09:49:23 +0200

    test(pass): player O wins when consecutive O are in row 3

--- src/components/Board.js
+++ src/components/Board.js
@@ -29,12 +29,13 @@ const {
 
 const checkOIsWinner = boardState =>
   hasWinningCombination(boardState, OInSameRow1) ||
-  hasWinningCombination(boardState, OInSameRow2)
+  hasWinningCombination(boardState, OInSameRow2) ||
+  hasWinningCombination(boardState, OInSameRow3)
 //  hasWinningCombination(boardState, OInSameCol1)
 // hasWinningCombination(boardState, OInSameCol2) ||
 // hasWinningCombination(boardState, OInSameCol3) ||
 // ||
-// hasWinningCombination(boardState, OInSameRow3) ||
+//  ||
 // hasWinningCombination(boardState, ODiagonalL) ||
 // hasWinningCombination(boardState, ODiagonalR)
 
