46a030f8c6013fac66c69a981d211705be64a7e8
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-05 18:10:26 +0200

    test(failed): add failed test. * The game must freeze when any player wins * This is done by checking if all buttons have been disabled once a game is won by any player

--- src/_tests_/FreezeAndReset.test.js
+++ src/_tests_/FreezeAndReset.test.js
@@ -1,11 +1,18 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import App from '../App'
+import { consecutiveXInRow } from './testData'
 
 describe('Freeze', () => {
   describe('when any player wins', () => {
-    test('the game freezes', () => {
+    test('the game freezes', async () => {
       render(<App />)
-      
+      consecutiveXInRow('R1', 'R2')
+      const allBlocks = screen.getAllByRole('button')
+      const isDeactivated = allBlocks.every(element => !element.disabled)
+      await waitFor(() => {
+        expect(screen.getByText('Winner is')).toBe(true)
+        expect(isDeactivated).toBe(true)
+      })
     })
     test('a start again button must appear', () => {})
   })
