23649208a68ec2197415a41eeb97ecc85a317719
Nidhi Sharma <nidhidsharma08121988@gmail.com>
2022-04-08 11:20:10 +0200

    test(failed): add failed test that when any player wins, game should freeze

--- src/_tests_/FreezeAndReset.test.js
+++ src/_tests_/FreezeAndReset.test.js
@@ -1,16 +1,24 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import App from '../App'
-import { consecutiveXInRow } from './testData'
+import { consecutiveXInRow, consecutiveOInRow } from './testData'
 
 describe('Freeze', () => {
   describe('when any player wins', () => {
-    test('the game freezes', async () => {
+    test('the game freezes when x wins', async () => {
       render(<App />)
       consecutiveXInRow('R1', 'R2')
       const allBlocks = screen.getAllByRole('button')
-      const isDeactivated = allBlocks.every(element => !element.disabled)
+      const isDeactivated = allBlocks.every(element => !element.style.disabled)
       await waitFor(() => {
-        expect(screen.getByText('Winner is')).toBe(true)
+        expect(isDeactivated).toBe(true)
+      })
+    })
+    test('the game freezes when o wins', async () => {
+      render(<App />)
+      consecutiveOInRow('R1')
+      const allBlocks = screen.getAllByRole('button')
+      const isDeactivated = allBlocks.every(element => !element.style.disabled)
+      await waitFor(() => {
         expect(isDeactivated).toBe(true)
       })
     })
